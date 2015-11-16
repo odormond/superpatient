619d9eecc64cc67f67b8013061fb6bba4601fed0
Olivier Dormond <olivier.dormond@gmail.com>
2015-11-16 07:01:13 +0100

    checksum for bvr

---
+++ bp_bvr.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python2
+# coding:UTF-8
+
+
+def checksum(n):
+    s = 0
+    for i in str(n):
+        s = [0, 9, 4, 6, 8, 2, 7, 1, 3, 5][(s+ord(i)-ord('0')) % 10]
+    return (10-s) % 10
