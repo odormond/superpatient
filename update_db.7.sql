7d79b1de6825e3be275f35005ed71e27a1db74ef
Olivier Dormond <olivier.dormond@gmail.com>
2017-02-07 20:50:29 +0100

    Fix update now that I know which one is the most frequent

--- update_db.7.sql
+++ update_db.7.sql
@@ -1,4 +1,5 @@
+UPDATE consultations set paye_le = null WHERE id_consult = 17880;
 ALTER TABLE consultations ADD COLUMN prix_txt text AFTER prix_cts;
 UPDATE consultations SET prix_txt = (SELECT description FROM tarifs WHERE tarifs.prix_cts = consultations.prix_cts);
 ALTER TABLE consultations ADD COLUMN majoration_txt text AFTER majoration_cts;
-UPDATE consultations SET majoration_txt = (SELECT description FROM majorations WHERE majorations.prix_cts = consultations.majoration_cts LIMIT 1);
+UPDATE consultations SET majoration_txt = (SELECT description FROM majorations WHERE majorations.prix_cts = consultations.majoration_cts ORDER BY description DESC LIMIT 1);
