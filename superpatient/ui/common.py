d402934980e0d7f255e3ec55c8cd1efb21b9cef9
Olivier Dormond <olivier.dormond@gmail.com>
2017-12-12 21:26:18 +0100

    Use the apropos and license string from the customization instead of hardcoded values.

--- superpatient/ui/common.py
+++ superpatient/ui/common.py
@@ -33,11 +33,12 @@ class AboutDialog(wx.Dialog):
 
     def __set_properties(self):
         self.SetTitle(u"À propos")
+        self.SetFont(wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.BOLD, 0, ""))
 
     def __do_layout(self):
+        from ..customization import labels_text
         sizer_3 = wx.BoxSizer(wx.VERTICAL)
-        label_15 = wx.StaticText(self, wx.ID_ANY, u"SuperPatient ver. 4.0 est un gestionnaire de patients,\nde consultations et de facturations.\n\nIl a été créé en 2006 pour satisfaire aux besoins\nminimaux d'un cabinet de groupe d'ostéopathes.\n\nSuperPatient est sous licence GPL.\n\nPour tout autre renseignement, veuillez écrire à\n\nTibor Csernay\ncsernay@pog.swiss", style=wx.ALIGN_CENTER)
-        label_15.SetFont(wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.BOLD, 0, ""))
+        label_15 = wx.StaticText(self, wx.ID_ANY, labels_text.apropos_description, style=wx.ALIGN_CENTER)
         sizer_3.Add(label_15, 1, wx.ALL | wx.EXPAND, 5)
         self.SetSizer(sizer_3)
         sizer_3.Fit(self)
@@ -57,8 +58,9 @@ class LicenseDialog(wx.Dialog):
         self.SetFont(wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.BOLD, 0, ""))
 
     def __do_layout(self):
+        from ..customization import labels_text
         sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        label_1 = wx.StaticText(self, wx.ID_ANY, u"POG Sàrl - Copyright 2006-2017 - www.pog.swiss\n\nSuperPatient is free software; you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation; either version 2 of the License, or\n(at you option) any later version.\n\nSuperPatient is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without event the implied warranty of\nMECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public LIcense\nalong with SuperPatient; if not, write to the Free Software\nFoundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA", style=wx.ALIGN_CENTER)
+        label_1 = wx.StaticText(self, wx.ID_ANY, labels_text.licence_description, style=wx.ALIGN_CENTER)
         sizer_1.Add(label_1, 1, wx.ALL | wx.EXPAND, 5)
         self.SetSizer(sizer_1)
         sizer_1.Fit(self)
