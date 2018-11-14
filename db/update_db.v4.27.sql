f9bfd5892c3f54af26b0b2cbe5b5915eefcbf8d2
Olivier Dormond <olivier.dormond@gmail.com>
2018-11-14 22:31:58 +0100

    Add indexes on the sites columns of the patients and consultations

--- db/update_db.v4.27.sql
+++ db/update_db.v4.27.sql
@@ -2,5 +2,8 @@ ALTER TABLE bills ALTER COLUMN site DROP DEFAULT;
 ALTER TABLE patients ALTER COLUMN site DROP DEFAULT;
 ALTER TABLE consultations ALTER COLUMN site DROP DEFAULT;
 
+CREATE INDEX patients_site_idx ON patients (site);
+CREATE INDEX consultations_site_idx ON consultations (site);
+
 CREATE TABLE version_info (db_version int);
 INSERT INTO version_info VALUES (1);
